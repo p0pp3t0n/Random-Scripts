2233c5154fb0443f481ae5a99f531fa8b8d72c74
p0pp3t <p0pp3t@gmail.com>
2016-04-25 19:42:09 -0400

    deleted:    Python/Web_Source/source 	deleted:    Python/Web_Source/test.py 	modified:   Python/Web_Source/test2.py 	Python/Web_Source/fetch.sh 	Python/Web_Source/research.py 	Python/Web_Source/search.py 	Python/Web_Source/test3.py

--- Python/Web_Source/test.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-for i in range(1,3):
-    page='http://www.novelashdgratis.tv/ver/la-esclava-blanca-capitulo-'+str(i)
-    print (page)
-    webpage=urllib2.urlopen(page)
-    html=webpage.read()
-
-    print(html)
